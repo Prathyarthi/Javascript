5ef1821eaffae5e6c45d50c971bc647fb96a7865
Prathyarthi <prathyarti@gmail.com>
2023-11-10 20:22:51 +0530

    Added datatypes

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+let a = "hello"
+let b = 23
+let c = true
+let d;
+let e = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);       // Note : null is a object
