9e61dc827f24db1481117c2554c0a49754f042db
Prathyarthi <prathyarti@gmail.com>
2023-11-17 15:23:28 +0530

    Added confusing operations

---
+++ operations.js
@@ -0,0 +1,17 @@
+let value = 6
+negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);           // If string is first, all the other will get converted to string
+console.log(1 + "2");     // If string is last, first expression will be solved and then string will be concatenated
+console.log("1" + 2 + 2);     // If string is first, all the other will get converted to string
+console.log(1 + 2 + "2"); // If string is last, first expression will be solved and then string will be concatenated
+
+console.log(+true);
+console.log(+"");
