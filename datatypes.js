152ca62cb459a244d0b921b386cc556d7cfa4619
Prathyarthi <prathyarti@gmail.com>
2023-11-20 06:29:43 +0530

    Datatypes summary!

--- datatypes.js
+++ datatypes.js
@@ -9,3 +9,30 @@ console.log(typeof b);
 console.log(typeof c);
 console.log(typeof d);
 console.log(typeof e);       // Note : null is a object
+
+// Primitive
+// String,Number,Boolean,null,undefined,Symbol,BigInt
+// Examples
+const score = 2
+const scoreValue = 2.5
+const isLoggedIn = false
+const temperature = null
+let userEmail;     // undefined
+const id = Symbol('123')      // To make it unique
+const bigNumber = 7214361543487261n   // bigInt
+
+// Referene(Non-Primitive)
+// Array,Objects,Functions
+
+const fruits = ["jackfruit", "Mango", "Guava"]
+let obj = {
+    name: "Prathyarthi",
+    age: 19
+}
+
+const func = function () {
+    console.log("Hello");
+}
+
+
+// Note : All non-primitive datatypes type is returned as object,and also null is a object as it is unique
