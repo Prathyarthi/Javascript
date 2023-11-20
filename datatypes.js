1f6cb08b2e79d26412e96c2dabcc81d0bd832bff
Prathyarthi <prathyarti@gmail.com>
2023-11-20 07:11:29 +0530

    Stack and Heap memory

--- datatypes.js
+++ datatypes.js
@@ -36,3 +36,26 @@ const func = function () {
 
 
 // Note : All non-primitive datatypes type is returned as object,and also null is a object as it is unique
+
+// Stack(primitive) and Heap(non-primitive) : 
+let name1 = "Amogh"
+let name2 = name1
+name2 = "Hello"
+console.log(name1);
+console.log(name2);
+
+// Primitive datatypes will be the call by value. Here name2 will get the copy of name1, so when we change name2, the original(name1) will not get changed
+
+let user1 = {
+    email: "hello@gmail.com"
+}
+
+let user2 = user1
+user2.email = "helloworld@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+// Non-Primitive datatypes will be the call by reference. Here user2 will get the reference of user1 directly, so when we change user2, the original(name1) will also be changed because direct reference of the user1 is given
+
+// Detailed notes in notion
